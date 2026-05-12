a18502a48209b0f3632dd699a20b1fe0160f9f7e
niihash <50837663+niihash@users.noreply.github.com>
2026-05-12 15:56:33 -0300

    feat: add api docs

--- app/Http/Controllers/Api/Tenant/AuthController.php
+++ app/Http/Controllers/Api/Tenant/AuthController.php
@@ -45,6 +45,6 @@ class AuthController extends Controller
 
     public function me(Request $request)
     {
-        return response()->json($request->user());
+        return response()->json(Auth::user());
     }
 }
--- config/tenancy.php
+++ config/tenancy.php
@@ -17,6 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'example.test',
         '127.0.0.1',
         'localhost',
     ],
